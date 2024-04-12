db7bed7049f1cb2fe5a6ac9897a50a1180834424
Said Mrini <161074055+smrini@users.noreply.github.com>
2024-04-12 22:15:27 +0200

    Add files via upload

---
+++ 20keypad/v1/rules.mk
@@ -0,0 +1,14 @@
+# Build Options
+# Yes = enable, No = disable
+
+BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = yes        # Console for debug
+COMMAND_ENABLE = yes        # Commands for debug and configuration
+NKRO_ENABLE = no            # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+AUDIO_ENABLE = no           # Audio output
+RGBLIGHT_ENABLE = yes		# Enable keyboard RGB underglow
+WS2812_DRIVER_REQUIRED = yes# required for RGBLIGHT to work
+WS2812_DRIVER = vendor		# required for RGBLIGHT to work
